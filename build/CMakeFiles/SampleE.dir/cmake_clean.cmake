04d3ed486f58776f45347fc60a3a011d6aa72c4d
denn1s <dennis.aldana@gmail.com>
2022-06-28 11:43:46 -0600

    Initial commit

---
+++ build/CMakeFiles/SampleE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SampleE.dir/src/main.cpp.o"
+  "CMakeFiles/SampleE.dir/src/main.cpp.o.d"
+  "SampleE"
+  "SampleE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SampleE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
