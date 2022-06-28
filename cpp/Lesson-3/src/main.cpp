75e7d1dbaacd37d43ad050b95c2313560e150dcd
denn1s <dennis.aldana@gmail.com>
2022-06-28 16:11:53 -0600

    glfw

--- cpp/Lesson-3/src/main.cpp
+++ cpp/Lesson-3/src/main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>	
-#include "Math/math.h"
+#include <GLFW/glfw3.h>
+// #include "Math/math.h"
 
 // #include "math.cpp"
 
 // we need a header if we dont include
 // float add(float a, float b);
 
+GLFWwindow *window;
+
 int main(int argc, const char * argv[]) {
     std::cout << "Hello World!" << std::endl;
 
-    std::cout << add(1.1f, 2.2f) << std::endl;
+    if (!glfwInit())
+    {
+      fprintf(stderr, "Failed to initialize\n");
+      exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(300, 300, "Sample", NULL, NULL);
+
+    if (!window)
+    {
+      fprintf(stderr, "Failed to open window\n");
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+
+
 
     return 0;
 } 
