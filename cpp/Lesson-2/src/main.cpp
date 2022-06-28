798393e2140bc7811c68f84c1bb6d0bc7b179e37
denn1s <dennis.aldana@gmail.com>
2022-06-28 12:46:11 -0600

    library

---
+++ cpp/Lesson-2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>	
+#include "Math/math.h"
+
+// #include "math.cpp"
+
+// we need a header if we dont include
+// float add(float a, float b);
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << add(1.1f, 2.2f) << std::endl;
+
+    return 0;
+} 
+
